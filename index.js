493d891495f60e4524d2ab9fae2cd273eee27ee5
Nishikant Bawache <nishikant.bawache.nb@gmail.com>
2023-01-08 21:32:25 +0530

    created a bubblePack diagram

--- index.js
+++ index.js
@@ -30,10 +30,47 @@ const svg = d3.select('.canvas')
 const graph = svg.append('g')
     .attr('transform', 'translate(50, 50)'); //to give a 50px margin
 
-// strategy and hierarchy
+// strategy and hierarchy, for data destructuring
 
 const stratify = d3.stratify()
     .id(d => d.name)
     .parentId(d => d.parent)
 
-console.log(stratify(data))
+const rootNode = stratify(data)
+    .sum(d => d.amount);
+
+// for creatig the data for bubblePack, just like how we do it for arc that
+// spits out angles for us
+const pack = d3.pack()
+    .size([960, 700])
+    .padding(5)
+
+// the pack method returns the data with r, cx, cy values computed
+// for the circle/bubble. However, when we want to pass the data
+// for creating the shape, we need it in array format.
+// that is when descendants come into play.
+const bubbleData = pack(rootNode).descendants();
+
+// create ordinal scale for colors
+const color = d3.scaleOrdinal(['#d1c4e9', '#b39ddb', '#9575cd']);
+
+//  join data and add group for each node
+const nodes = graph.selectAll('g')
+    .data(bubbleData)
+    .enter()
+    .append('g')
+    .attr('transform', d => `translate(${d.x}, ${d.y})`)
+
+nodes.append('circle')
+    .attr('r', d => d.r)
+    .attr('stroke', 'white')
+    .attr('stroke-width', 2)
+    .attr('fill', d => color(d.depth));
+
+nodes.filter(d => !d.children)
+    .append('text')
+    .attr('text-anchor', 'middle')
+    .attr('dy', '0.3em')
+    .attr('fill', 'white')
+    .style('font-size', d => d.value * 5)
+    .text(d => d.data.name)
