640c414e9a17710b0e00a5513e7c3c52831c9183
Nishikant Bawache <nishikant.bawache.nb@gmail.com>
2023-01-08 21:05:53 +0530

    basic data structuring

--- index.js
+++ index.js
@@ -1,47 +1,39 @@
-const btns = document.querySelectorAll('button');
-const form = document.querySelector('form');
-const formAct = document.querySelector('form span');
-const input = document.querySelector('input');
-const error = document.querySelector('.error');
+const data = [
+    { name: 'news', parent: '' },
+    { name: 'tech', parent: 'news' },
+    { name: 'sport', parent: 'news' },
+    { name: 'music', parent: 'news' },
+    { name: 'ai', parent: 'tech', amount: 7 },
+    { name: 'coding', parent: 'tech', amount: 5 },
+    { name: 'tablets', parent: 'tech', amount: 4 },
+    { name: 'laptops', parent: 'tech', amount: 6 },
+    { name: 'd3', parent: 'tech', amount: 3 },
+    { name: 'gaming', parent: 'tech', amount: 3 },
+    { name: 'football', parent: 'sport', amount: 6 },
+    { name: 'hockey', parent: 'sport', amount: 3 },
+    { name: 'baseball', parent: 'sport', amount: 5 },
+    { name: 'tennis', parent: 'sport', amount: 6 },
+    { name: 'f1', parent: 'sport', amount: 1 },
+    { name: 'house', parent: 'music', amount: 3 },
+    { name: 'rock', parent: 'music', amount: 2 },
+    { name: 'punk', parent: 'music', amount: 5 },
+    { name: 'jazz', parent: 'music', amount: 2 },
+    { name: 'pop', parent: 'music', amount: 3 },
+    { name: 'classical', parent: 'music', amount: 5 },
+];
 
-let activity = 'cycling'
+const svg = d3.select('.canvas')
+    .append('svg')
+    .attr('width', 1060)
+    .attr('height', 800)
 
-btns.forEach(btn => {
-    btn.addEventListener('click', e => {
-        // get activity
-        activity = e.target.dataset.activity;
+const graph = svg.append('g')
+    .attr('transform', 'translate(50, 50)'); //to give a 50px margin
 
-        // remove and add active class
-        btns.forEach(btn => btn.classList.remove('active'));
-        e.target.classList.add('active');
+// strategy and hierarchy
 
-        // set ID of input foeld
-        input.setAttribute('id', activity);
+const stratify = d3.stratify()
+    .id(d => d.name)
+    .parentId(d => d.parent)
 
-        // set text of formSpan
-        formAct.textContent = activity;
-
-        // call the update funciton
-        update(data);
-    })
-});
-
-// form submit
-
-form.addEventListener('submit', e => {
-    e.preventDefault();
-
-    const distance = parseInt(input.value);
-    if (distance) {
-        db.collection('activities').add({
-            distance,
-            activity,
-            date: new Date().toString()
-        }).then(() => {
-            input.value = '';
-            error.textContent = 'Sucessfully submitted';
-        })
-    } else {
-        error.textContent = 'Please enter a valid distance in meters.'
-    }
-})
+console.log(stratify(data))
